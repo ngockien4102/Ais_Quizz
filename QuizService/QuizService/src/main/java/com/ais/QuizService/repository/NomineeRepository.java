4c15367106650414f4ec81d4f27a169536e1e100
ngockien <nguyenngockien41020@gmail.com>
2022-01-25 15:51:03 +0700

    update

--- QuizService/QuizService/src/main/java/com/ais/QuizService/repository/NomineeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ais.QuizService.repository;
-
-import com.ais.QuizService.entity.NomineeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NomineeRepository extends JpaRepository<NomineeEntity,Long> {
-}
--- QuizService/QuizService/src/main/java/com/ais/QuizService/repository/QuestionTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ais.QuizService.repository;
-
-import com.ais.QuizService.entity.QuestionTypeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionTypeRepository extends JpaRepository<QuestionTypeEntity, Long> {
-}
